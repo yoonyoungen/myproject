baaf8281a33afd75971e2ec068831bbe91585c02
admin <admin@DESKTOP-P0IP4GB>
2022-11-01 14:14:33 +0900

    HelloWorld 작성

---
+++ src/main/java/yoon/web/project/App.java
@@ -0,0 +1,13 @@
+package yoon.web.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
